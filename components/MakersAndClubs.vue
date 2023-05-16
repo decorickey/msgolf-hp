d5121bae3a9231e1ec5d297e764f640bc6b0ef1c
decorickey <rikiya.shikida@gmail.com>
2023-05-16 22:13:30 +0900

    add brands

--- components/MakersAndClubs.vue
+++ components/MakersAndClubs.vue
@@ -1,4 +1,69 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface Brand {
+  name: string;
+  clubs?: Club[];
+}
+interface Club {
+  images: string[];
+  label: string;
+  slide: number;
+}
+
+const brands = reactive<Brand[]>([
+  {
+    name: "afd",
+    clubs: [
+      {
+        images: ["club-1-1.jpg", "club-1-2.jpg"],
+        label: "あああ",
+        slide: 1,
+      },
+      {
+        images: ["club-2-1.jpg"],
+        label: "いいい",
+        slide: 1,
+      },
+    ],
+  },
+  {
+    name: "baldo",
+    clubs: [
+      {
+        images: ["club-1-1.jpg"],
+        label: "あああ",
+        slide: 1,
+      },
+    ],
+  },
+  {
+    name: "buddy",
+  },
+  {
+    name: "forebes",
+  },
+  {
+    name: "gorilla",
+  },
+  {
+    name: "jbeam",
+  },
+  {
+    name: "kyoei",
+  },
+  {
+    name: "maxsoul",
+  },
+  {
+    name: "miura",
+  },
+  {
+    name: "mystery",
+  },
+  {
+    name: "romaro",
+  },
+])
+</script>
 
 <template>
   <div>
@@ -6,11 +71,64 @@
       取り扱いメーカー
     </h2>
 
-    <p>様々なメーカーのゴルフクラブを取り扱っております。</p>
+    <p class="text-center">
+      様々なメーカーのゴルフクラブを取り扱っております。
+    </p>
 
     <div class="row">
-      <div class="col">
-        col
+      <div
+        v-for="(brand, i) of brands"
+        :key="i"
+        class="col-12 col-md-4"
+      >
+        <q-list bordered>
+          <q-expansion-item
+            class="box-shadow-1"
+            group="makers"
+            header-class="q-pr-none"
+          >
+            <template #header>
+              <q-img
+                class="q-mr-sm"
+                :src="`brands/${brand.name}/logo.png`"
+              />
+            </template>
+
+            <q-card>
+              <q-card-section>
+                <div class="row">
+                  <div
+                    v-for="(club, j) of brand.clubs"
+                    :key="j"
+                    class="col-12"
+                  >
+                    <q-responsive :ratio="4/3">
+                      <q-carousel
+                        v-model="club.slide"
+                        animated
+                        arrows
+                        autoplay
+                        navigation
+                        infinite
+                      >
+                        <q-carousel-slide
+                          v-for="(image, k) of club.images"
+                          :key="k"
+                          :name="k"
+                          :img-src="`brands/${brand.name}/${image}`"
+                        />
+                      </q-carousel>
+                    </q-responsive>
+
+                    <p class="text-center q-mt-md">
+                      {{ club.label }}
+                    </p>
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </q-list>
       </div>
     </div>
   </div>
